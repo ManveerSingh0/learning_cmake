f806be6c82099c7b9cc47561820c302c628fea4a
ManveerSingh0 <singhcoder2005@gmail.com>
2024-02-07 14:45:39 +0530

    learning cmakelist

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "player.h"
+
+
+int main(){
+    Player manveer;
+    manveer.talk();
+    return 0;
+}
---
+++ player.cpp
@@ -0,0 +1,9 @@
+#include "player.h"
+#include <iostream>
+
+
+void Player::talk(){
+    std::cout << "Hello World" << std::endl;
+    return;
+}
+
