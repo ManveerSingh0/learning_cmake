c5c6062a5c5f116288ef36be2ab990bc0c57f588
ManveerSingh0 <singhcoder2005@gmail.com>
2024-02-08 11:01:23 +0530

    learned about config files and other commands

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "player.h"
+#include "config.hpp"
 
 
 int main(){
     Player manveer;
     manveer.talk();
+    std::cout << project_name << std::endl;
+    std::cout << project_version << std::endl;
     return 0;
 }
