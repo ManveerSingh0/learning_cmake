3b49f1e0ec6c4e2fa471138b096a15e6cbb300aa
ManveerSingh0 <singhcoder2005@gmail.com>
2024-03-15 23:05:42 +0530

    learned about version name in cpp and there is another file extension for cpp for cmake config.hpp.in

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include "my_lib.h"
+#include "config.hpp"
+
 
 
 int main(){
-  My_lib object;
-  object.say_hello();
+  // My_lib object;
+  // object.say_hello();
+
+
+  std::cout << project_version << " " << project_name << std::endl;
+  std::cout << project_version_major << " " << project_version_minor << " " << project_version_patch << std::endl;
   return 0;
 }
