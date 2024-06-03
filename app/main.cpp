948b393eb460e4a9cc56262e04878e952929ba77
ManveerSingh0 <singhcoder2005@gmail.com>
2024-06-03 19:38:26 +0530

    nothing

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "my_lib.h"
+#include "config.hpp"
 
 
 int main(){
+  std::cout << project_name << '\n' << project_version << std::endl;
   My_lib object;
   object.say_hello();
   return 0;
