ee3165f98f911a4dbd8df90a9af85fa2baf42b67
ManveerSingh0 <singhcoder2005@gmail.com>
2024-07-22 13:52:52 +0530

    nothing

---
+++ build/CMakeFiles/learning_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learning_cmake.dir/main.cpp.o"
+  "CMakeFiles/learning_cmake.dir/main.cpp.o.d"
+  "learning_cmake"
+  "learning_cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learning_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
